2b7a0a224a8f579dad21d65091c111bd2847b623
Steven Perilla <stiven.perilla@outlook.com>
2024-10-17 01:00:05 +0000

    Templates

--- python-container/app.py
+++ python-container/app.py
@@ -1,10 +1,11 @@
 from flask import Flask  
+from django import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello Word"
+    return render_template ('index.html')
 
 if __name__=='__main__':
     app.run(host='0.0.0.0', debug=True)
